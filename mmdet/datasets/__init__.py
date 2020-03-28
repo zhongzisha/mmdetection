21a90fc0b222957387cdd453b939f9849b687996
zhongzisha <zhongzisha@outlook.com>
2020-03-28 19:38:52 +0800

    fix datast

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -9,9 +9,20 @@ from .voc import VOCDataset
 from .wider_face import WIDERFaceDataset
 from .xml_style import XMLDataset
 
+from .extra_aug import ExtraAugmentation
+from .DOTA import DOTADataset, DOTADataset_v3
+from .DOTA2 import DOTA2Dataset
+from .DOTA2 import DOTA2Dataset_v2
+from .DOTA2 import DOTA2Dataset_v3, DOTA2Dataset_v4
+from .HRSC import HRSCL1Dataset
+from .DOTA1_5 import DOTA1_5Dataset, DOTA1_5Dataset_v3, DOTA1_5Dataset_v2
+
 __all__ = [
     'CustomDataset', 'XMLDataset', 'CocoDataset', 'VOCDataset',
     'CityscapesDataset', 'GroupSampler', 'DistributedGroupSampler',
     'build_dataloader', 'ConcatDataset', 'RepeatDataset', 'WIDERFaceDataset',
-    'DATASETS', 'build_dataset'
+    'DATASETS', 'build_dataset',
+    'ExtraAugmentation', 'HRSCL1Dataset', 'DOTADataset_v3',
+    'DOTA1_5Dataset', 'DOTA1_5Dataset_v3', 'DOTA1_5Dataset_v2',
+    'DOTA2Dataset_v4'
 ]
