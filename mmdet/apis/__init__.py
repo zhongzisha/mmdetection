44b71606a4e989eddbd6db54b613aa60e4119680
zhongzisha <zhongzisha@outlook.com>
2020-03-28 22:54:14 +0800

    do test

--- mmdet/apis/__init__.py
+++ mmdet/apis/__init__.py
@@ -1,10 +1,12 @@
 from .inference import (async_inference_detector, inference_detector,
-                        init_detector, show_result, show_result_pyplot)
+                        init_detector, show_result, show_result_pyplot,
+                        draw_poly_detections)
 from .test import multi_gpu_test, single_gpu_test
 from .train import get_root_logger, set_random_seed, train_detector
 
 __all__ = [
     'get_root_logger', 'set_random_seed', 'train_detector', 'init_detector',
     'async_inference_detector', 'inference_detector', 'show_result',
-    'show_result_pyplot', 'multi_gpu_test', 'single_gpu_test'
+    'show_result_pyplot', 'multi_gpu_test', 'single_gpu_test',
+    'draw_poly_detections'
 ]
--- mmdet/apis/inference.py
+++ mmdet/apis/inference.py
@@ -205,3 +205,42 @@ def show_result_pyplot(img,
         img, result, class_names, score_thr=score_thr, show=False)
     plt.figure(figsize=fig_size)
     plt.imshow(mmcv.bgr2rgb(img))
+
+
+def draw_poly_detections(img, detections, class_names, scale, threshold=0.2):
+    """
+    :param img:
+    :param detections:
+    :param class_names:
+    :param scale:
+    :param cfg:
+    :param threshold:
+    :return:
+    """
+    import pdb
+    import cv2
+    import random
+    assert isinstance(class_names, (tuple, list))
+    img = mmcv.imread(img)
+    color_white = (255, 255, 255)
+
+    for j, name in enumerate(class_names):
+        color = (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256))
+        try:
+            dets = detections[j]
+        except:
+            pdb.set_trace()
+        for det in dets:
+            bbox = det[:8] * scale
+            score = det[-1]
+            if score < threshold:
+                continue
+            bbox = list(map(int, bbox))
+
+            cv2.circle(img, (bbox[0], bbox[1]), 3, (0, 0, 255), -1)
+            for i in range(3):
+                cv2.line(img, (bbox[i * 2], bbox[i * 2 + 1]), (bbox[(i+1) * 2], bbox[(i+1) * 2 + 1]), color=color, thickness=2)
+            cv2.line(img, (bbox[6], bbox[7]), (bbox[0], bbox[1]), color=color, thickness=2)
+            cv2.putText(img, '%s %.3f' % (class_names[j], score), (bbox[0], bbox[1] + 10),
+                        color=color_white, fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=0.5)
+    return img
