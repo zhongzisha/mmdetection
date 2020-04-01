ea85997a9bae2f7548c210302a111f10b2c62cf5
zhongzisha <zhongzisha@outlook.com>
2020-04-01 21:52:47 +0800

    try to fix

--- mmdet/core/anchor/__init__.py
+++ mmdet/core/anchor/__init__.py
@@ -4,6 +4,7 @@ from .anchor_target import (anchor_inside_flags, anchor_target,
 from .guided_anchor_target import ga_loc_target, ga_shape_target
 from .point_generator import PointGenerator
 from .point_target import point_target
+from .anchor_target_rbbox import anchor_target_rbbox
 
 __all__ = [
     'AnchorGenerator', 'anchor_target', 'anchor_inside_flags', 'ga_loc_target',
