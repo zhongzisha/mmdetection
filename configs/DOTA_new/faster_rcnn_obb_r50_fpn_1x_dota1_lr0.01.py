ebab4c55a7889da2e8aaebe8a89219d4a2e5d079
zhongzisha <zhongzisha@outlook.com>
2020-04-04 02:00:34 +0800

    fix

--- configs/DOTA_new/faster_rcnn_obb_r50_fpn_1x_dota1_lr0.01.py
+++ configs/DOTA_new/faster_rcnn_obb_r50_fpn_1x_dota1_lr0.01.py
@@ -185,7 +185,7 @@ log_config = dict(
 total_epochs = 12
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/faster_rcnn_obb_r50_fpn_1x_dota1'
+work_dir = './work_dirs/faster_rcnn_obb_r50_fpn_1x_dota1_lr0.01'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
