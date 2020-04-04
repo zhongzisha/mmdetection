ac12163dce370ddfec7ae9ee8e98143040ecec97
zhongzisha <zhongzisha@outlook.com>
2020-04-04 09:14:33 +0800

    add some configs

--- configs/DOTA_new/faster_rcnn_RoITrans_r50_fpn_1x_dota1_lr0.02.py
+++ configs/DOTA_new/faster_rcnn_RoITrans_r50_fpn_1x_dota1_lr0.02.py
@@ -197,7 +197,7 @@ lr_config = dict(
     warmup='linear',
     warmup_iters=500,
     warmup_ratio=1.0 / 3,
-    step=[8, 11, 20])
+    step=[8, 11])
 checkpoint_config = dict(interval=1)
 # yapf:disable
 log_config = dict(
@@ -208,7 +208,7 @@ log_config = dict(
     ])
 # yapf:enable
 # runtime settings
-total_epochs = 30  # 12
+total_epochs = 12
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
 work_dir = './work_dirs/faster_rcnn_RoITrans_r50_fpn_1x_dota1_lr0.02'
