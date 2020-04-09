6b41b1c55598082da7e7f4260609a56b9a3e62bd
zhongzisha <zhongzisha@outlook.com>
2020-04-09 15:58:24 +0800

    modified transforms for mask flip, pad and resize

--- configs/DOTA_new/retinanet_obb_360_r50_fpn_2x_dota1_lr0.01.py
+++ configs/DOTA_new/retinanet_obb_360_r50_fpn_2x_dota1_lr0.01.py
@@ -105,7 +105,7 @@ data = dict(
         pipeline=test_pipeline))
 evaluation = dict(interval=1, metric='bbox')
 # optimizer
-optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.0001, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
