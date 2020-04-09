1063a36e8174dcfd30652bd7e5eb62c3105ac2e2
zhongzisha <zhongzisha@outlook.com>
2020-04-09 19:40:43 +0800

    modified transforms for mask flip, pad and resize

--- mmdet/core/bbox/transforms_rbbox.py
+++ mmdet/core/bbox/transforms_rbbox.py
@@ -962,7 +962,8 @@ def hbb2obb_v2_360(boxes):
                           ex_ctr_y.unsqueeze(1),
                           ex_widths.unsqueeze(1),
                           ex_heights.unsqueeze(1)), 1)
-    initial_angles = c_bboxes.new_ones((num_boxes, 1)) * np.pi / 2
+    # initial_angles = c_bboxes.new_ones((num_boxes, 1)) * np.pi / 2
+    initial_angles = c_bboxes.new_zeros((num_boxes, 1))
     # initial_angles = -torch.ones((num_boxes, 1)) * np.pi/2
     dbboxes = torch.cat((c_bboxes, initial_angles), 1)
 
