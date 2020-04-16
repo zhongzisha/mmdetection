3759b2e1663956c7fb8bd12995d54ff490811524
zhongzisha <zhongzisha@outlook.com>
2020-04-16 20:38:54 +0800

    using get_best_begin_point

--- mmdet/core/bbox/transforms_rbbox.py
+++ mmdet/core/bbox/transforms_rbbox.py
@@ -870,7 +870,7 @@ def gt_mask_bp_obbs_360(gt_masks, with_module=True):
 
     # trans gt_masks to gt_obbs
     # gt_polys = mask2poly(gt_masks)
-    gt_bp_polys = get_best_begin_point(gt_masks)
+    gt_bp_polys = get_best_begin_point_360(gt_masks)
     # gt_obbs = polygonToRotRectangle_batch_360(gt_masks)
     gt_obbs = polygonToRotRectangle_batch(gt_bp_polys, with_module)
 
@@ -938,6 +938,42 @@ def get_best_begin_point(coordinate_list):
     best_coordinate_list = np.stack(list(best_coordinate_list))
 
     return best_coordinate_list
+
+
+def get_best_begin_point_360(quad_boxes):
+    new_quad_boxes = []
+    for quad_box in quad_boxes:
+        x1 = quad_box[0]
+        y1 = quad_box[1]
+        x2 = quad_box[2]
+        y2 = quad_box[3]
+        x3 = quad_box[4]
+        y3 = quad_box[5]
+        x4 = quad_box[6]
+        y4 = quad_box[7]
+        xmin = min(x1, x2, x3, x4)
+        ymin = min(y1, y2, y3, y4)
+        xmax = max(x1, x2, x3, x4)
+        ymax = max(y1, y2, y3, y4)
+        combinate = [[[x1, y1], [x2, y2], [x3, y3], [x4, y4]], [[x2, y2], [x3, y3], [x4, y4], [x1, y1]],
+                     [[x3, y3], [x4, y4], [x1, y1], [x2, y2]], [[x4, y4], [x1, y1], [x2, y2], [x3, y3]]]
+        dst_coordinate = [[xmin, ymin], [xmax, ymin], [xmax, ymax], [xmin, ymax]]
+        force = 100000000.0
+        force_flag = 0
+        for i in range(4):
+            temp_force = cal_line_length(combinate[i][0], dst_coordinate[0]) + \
+                         cal_line_length(combinate[i][1], dst_coordinate[1]) + \
+                         cal_line_length(combinate[i][2], dst_coordinate[2]) + \
+                         cal_line_length(combinate[i][3], dst_coordinate[3])
+            if temp_force < force:
+                force = temp_force
+                force_flag = i
+        if force_flag != 0:
+            pass
+            # print("choose one direction!")
+        new_quad_boxes.append(np.array(combinate[force_flag]).reshape((1, 8)))
+    return np.array(new_quad_boxes).reshape((-1, 8))
+
 
 # def polygonToRotRectangle(polys):
 #     """
