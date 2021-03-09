f2f7881555290abd607ef8f75e64b3b4a8ea4fc7
zhongzisha <zhongzisha@outlook.com>
2021-03-10 07:56:13 +0800

    gd1024 v1

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -12,6 +12,7 @@ from .utils import (NumClassCheckHook, get_loading_pipeline,
 from .voc import VOCDataset
 from .wider_face import WIDERFaceDataset
 from .xml_style import XMLDataset
+from .gd1024 import Gd1024Dataset
 
 __all__ = [
     'CustomDataset', 'XMLDataset', 'CocoDataset', 'DeepFashionDataset',
@@ -20,5 +21,6 @@ __all__ = [
     'DistributedSampler', 'build_dataloader', 'ConcatDataset', 'RepeatDataset',
     'ClassBalancedDataset', 'WIDERFaceDataset', 'DATASETS', 'PIPELINES',
     'build_dataset', 'replace_ImageToTensor', 'get_loading_pipeline',
-    'NumClassCheckHook'
+    'NumClassCheckHook',
+    'Gd1024Dataset'
 ]
