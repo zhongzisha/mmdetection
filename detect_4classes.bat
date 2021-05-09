dbd39927247f2bc3ada783abbb75dc93e42c2c3b
zhongzisha <zhongzisha@outlook.com>
2021-05-09 15:54:54 +0800

    try the newAug method

--- detect_4classes.bat
+++ detect_4classes.bat
@@ -2,8 +2,11 @@
 
 rem --cls-weights E:/cls_results/resnet50/bs32_lr0.001000_epochs20/best.pt
 set PYTHONPATH=F:\gd\
-set SUBSET=val
+set SUBSET=train
 set IMGSIZE=800
+set GAP=32
+rem set IMGSIZE=1024
+rem set GAP=256
 set NET=yolov5l_768_4classes
 set CONFIG=retinanet_r50_fpn_1x_coco
 set CONFIG=faster_rcnn_r50_fpn_ohem_1x_coco
@@ -26,10 +29,12 @@ set CONFIG=faster_rcnn_r50_fpn_dc5_2x_coco_lr0.001_nms2
 set CONFIG=faster_rcnn_r50_fpn_dc5_ohem_1x_coco_lr0.001_nms2_anchor1
 set CONFIG=faster_rcnn_r50_fpn_dc5_ohem_1x_coco_lr0.001_nms2_anchor2
 set CONFIG=faster_rcnn_r50_fpn_dc5_1x_coco_lr0.001
-set CONFIG=faster_rcnn_r50_fpn_dc5_1x_coco_lr0.001_800
+rem set CONFIG=faster_rcnn_r50_fpn_dc5_1x_coco_lr0.001_800
+rem set CONFIG=faster_rcnn_r50_fpn_dc5_1x_coco_lr0.0025_800_anchor1
+set CONFIG=faster_rcnn_r50_fpn_dc5_1x_coco_lr0.0025_800_anchor1_bs2
 
 set EPOCHNAME=latest
-set EPOCHNAME=epoch_11
+set EPOCHNAME=epoch_10
 
 
 python demo\detect_gd1024_4classes.py ^
@@ -40,15 +45,16 @@ python demo\detect_gd1024_4classes.py ^
     --iou-thres 0.5 ^
     --hw-thres 10 ^
     --img-size %IMGSIZE% ^
-    --gap 32 ^
+    --gap %GAP% ^
     --save-txt ^
     --device 0 ^
-    --project E:\mmdetection\work_dirs\%CONFIG%\outputs_%SUBSET%_%IMGSIZE%_%EPOCHNAME% ^
+    --project E:\mmdetection\work_dirs\%CONFIG%\outputs_%SUBSET%_%IMGSIZE%_%GAP%_%EPOCHNAME% ^
     --gt-xml-dir E:\gd_gt_combined_4classes\ ^
     --gt-subsize 5120 ^
     --gt-gap 128 ^
     --big-subsize 10240 ^
-    --batchsize 4
+    --batchsize 4 ^
+    --view-img
 
 
 
