20c6d73bb8ee54e889656de339b5807bc9e76e1b
zhongzisha <zhongzisha@outlook.com>
2021-07-11 21:55:04 +0800

    ad v5 and v4_albu

---
+++ configs/faster_rcnn/faster_rcnn_r50_fpn_1x_gd1024_v4_albu.py
@@ -0,0 +1,7 @@
+_base_ = [
+    '../_base_/models/faster_rcnn_r50_fpn_gd1024_rotate.py',
+    '../_base_/datasets/gd1024_detection_4classes_box_aug_v4_albu.py',
+    '../_base_/schedules/schedule_1x.py',
+    '../_base_/default_runtime.py'
+]
+optimizer = dict(lr=0.0025)
